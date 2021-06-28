ceffd1ec31bbcea17ce68fd01e673675ffd60741
Gleb Krasilich <gleb.chili@gmail.com>
2021-06-28 13:04:33 +0300

    Complete tests

--- Tests/SinkTest.cs
+++ Tests/SinkTest.cs
@@ -44,12 +44,14 @@ namespace Tests
                     string VerboseMessage1 = Guid.NewGuid().ToString();
                     string DebugMessage1 = Guid.NewGuid().ToString();
                     string InformationMessage1 = Guid.NewGuid().ToString();
+                    string WarningMessage1 = Guid.NewGuid().ToString();
                     string ErrorMessage1 = Guid.NewGuid().ToString();
                     string FatalMessage1 = Guid.NewGuid().ToString();
 
                     log1.Verbose(VerboseMessage1);
                     log1.Debug(DebugMessage1);
                     log1.Information(InformationMessage1);
+                    log1.Warning(WarningMessage1);
                     log1.Error(ErrorMessage1);
                     log1.Fatal(FatalMessage1);
 
@@ -60,6 +62,26 @@ namespace Tests
                     if (!Regex.IsMatch(console_log, @$"\[Verbose\].+{VerboseMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
                     {
                         throw new Exception("Verbose message 1 test failed");
+                    }
+                    if (!Regex.IsMatch(console_log, @$"\[Debug\].+{DebugMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
+                    {
+                        throw new Exception("Debug message 1 test failed");
+                    }
+                    if (!Regex.IsMatch(console_log, @$"\[Inforamtion\].+{InformationMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
+                    {
+                        throw new Exception("Information message 1 test failed");
+                    }
+                    if (!Regex.IsMatch(console_log, @$"\[Warning\].+{WarningMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
+                    {
+                        throw new Exception("Warning message 1 test failed");
+                    }
+                    if (!Regex.IsMatch(console_log, @$"\[Error\].+{ErrorMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
+                    {
+                        throw new Exception("Error message 1 test failed");
+                    }
+                    if (!Regex.IsMatch(console_log, @$"\[Fatal\].+{FatalMessage1}$", RegexOptions.ECMAScript | RegexOptions.Multiline | RegexOptions.Compiled))
+                    {
+                        throw new Exception("Fatal message 1 test failed");
                     }
 
                     File.WriteAllText("test-success.txt", "success");
